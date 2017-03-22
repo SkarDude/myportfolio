9d253b24d403b82e8e0b9c37b8823b6d25d72827
Alex Nagel <alexander.nagel@ucf.edu>
2017-03-22 11:14:42 -0400

    Cleaned up html, css and php to be correct indentation.

--- public_html/forms/contactform.php
+++ public_html/forms/contactform.php
@@ -1,11 +1,10 @@
 <?php
 if( !isset($_POST['g-recaptcha-response']) || trim($_POST['g-recaptcha-response']) === '' ){
-            echo "Please complete the reCAPTCHA";
-            die();
-        }
-        
-session_start();
+echo "Please complete the reCAPTCHA";
+die();
+}
 
+session_start();
 $msg='';
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
@@ -19,7 +18,7 @@ $ip=$_SERVER['REMOTE_ADDR'];
 $url=$google_url."?secret=".$secret."&response=".$recaptcha."&remoteip=".$ip;
 $res=getCurlData($url);
 $res= json_decode($res, true);
-//reCaptcha success check 
+//reCaptcha success check
 if($res['success'])
 {
 //Include login check code
@@ -28,39 +27,27 @@ else
 {
 $msg="Please re-enter your reCAPTCHA.";
 }
-
 }
 else
 {
 $msg="Please re-enter your reCAPTCHA.";
 }
-
 }
-
 $email = $_POST['email'];
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-  echo("$email is a valid email address");
-
+echo("$email is a valid email address");
 } else {
-  exit("Error: $email is not a valid email address please go back and try again");
-
-
+exit("Error: $email is not a valid email address please go back and try again");
 }
-
 $utf8 = $_POST['message'];
 $message = mb_convert_encoding($utf8, 'HTML-ENTITIES', 'UTF-8');
 $formcontent=" From: $email \n Message: $message";
 $recipient = "hey@alexnagel.me";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-
 echo '<script language="javascript">';
 echo 'alert("Message Sent")';
 echo '</script>';
 $success =mail($recipient, $subject, $formcontent, $mailheader) or exit("Error: message not sent correctly please go back and try again!");
-
-
-
-
 ?>
